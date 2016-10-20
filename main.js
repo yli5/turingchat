731f7112e9bfbe6472b4357fdf16df9d6460e805
jkim- <jaehongkim86@gmail.com>
2016-10-19 17:50:47 -0700

    Differentiated two types of chatrooms; one with bots and one with humans.

--- main.js
+++ main.js
@@ -2,47 +2,80 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-//var onlinePair = {};
-var onlineUser = [];
+var usernames = [];
+var usersWithBot = [];
 var UserRoomPair = {};
-var onlineCount = 0;
+var roomCount = 0;
+var botname = 'Alan'; // Make it change randomly?
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/chat.html');
 });
 
-
 io.on('connection', function(socket){
 	console.log('a user connected');
 
 	// listen for a new user:
-	if(onlineUser.length == 0){
-			onlineUser[0] = socket.id;
-			socket.join('room' + onlineCount.toString());
-			UserRoomPair[socket.id] = 'room' + onlineCount.toString();
-	}
-	else{
-		//onlinePair[onlineUser[0]] = socket.id;
-		//onlinePair[socket.id] = onlineUser[0];
-		//io.emit('login',{for:onlineUser[0]});
-		//console.log(obj.userid + 'enters the chat room');
-		socket.join('room' + onlineCount.toString());
-		UserRoomPair[socket.id] = 'room' + onlineCount.toString();
-		onlineCount++;
-		onlineUser = [];
-	}
+  socket.on('adduser', function(username){
+    socket.username = username;
+    // Assign to human chatroom
+    if (Math.random() < 0.5) {
+      socket.room = 'room' + roomCount.toString();
+      usernames.push(socket.username);
+      socket.join(socket.room);
+      UserRoomPair[socket.username] = socket.room;
+      if (usernames.length % 2 == 0){
+        roomCount++;
+      }
+    }
+    // Assign to bot chatroom
+    else {
+      socket.room = 'botroom' + usersWithBot.length.toString();
+      usersWithBot.push(socket.username);
+      socket.join(socket.room);
+      UserRoomPair[socket.username] = socket.room;
+    }
+    console.log(socket.username + ' has joined ' + socket.room);
+    console.log(usernames);
+    console.log(usersWithBot);
+    console.log(UserRoomPair);
+  });
 		
-
-	socket.on('chat message', function(msg){
-		//socket.emit('chat message', socket.id);
-		//socket.emit('chat message', onlineUser);
-		//socket.emit('chat message', UserRoomPair[socket.id]);
-		socket.emit('chat message', msg);
-		socket.broadcast.to(UserRoomPair[socket.id]).emit('chat message', msg);
+  // broadcast new message
+	socket.on('sendchat', function(msg){
+		io.sockets.in(socket.room).emit('updatechat', socket.username, msg);
+    // If user is in a bot room, bot responds
+    if (usernames.indexOf(socket.username) == -1) {
+      if (Math.random() < 0.5) {
+        botMsg = 'Yes'
+      }
+      else {
+        botMsg = 'No'
+      }
+      io.sockets.in(socket.room).emit('updatechat', botname, botMsg);
+    }
 	});
 
+  // disconnect
+  socket.on('disconnect', function(){
+    usrIdx = usernames.indexOf(socket.username)
+    if (usrIdx != -1) {
+      usernames.splice(usrIdx, 1);
+      if (usernames.length % 2 != 0) {
+        roomCount--;
+      }
+    }
+    else {
+      usersWithBot.splice(usersWithBot.indexOf(socket.username));
+    }
+    delete UserRoomPair[socket.username];
+    socket.leave(socket.room);
+    socket.emit('chat message', 'SERVER', socket.username+' has disconnected');
+    console.log(socket.username+' disconnected');
+  });
 });
 
+
 http.listen(3000, function(){
-	console.log('listening on *.3000');
+	console.log('listening on *:3000');
 });
